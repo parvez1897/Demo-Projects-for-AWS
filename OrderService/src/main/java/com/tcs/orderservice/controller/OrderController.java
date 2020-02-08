24683fdf2729e990fdee89e3da276a682d321b56
parvez1897 <sheikhparvez10@gmail.com>
2020-02-09 01:38:23 +0530

    new commit

--- OrderService/src/main/java/com/tcs/orderservice/controller/OrderController.java
+++ OrderService/src/main/java/com/tcs/orderservice/controller/OrderController.java
@@ -1,17 +1,22 @@
 package com.tcs.orderservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 @RequestMapping("/order")
 public class OrderController {
 	
+	@Value("${msg}")
+	String msg;
 	
 	@GetMapping("/hello")
 	public String hello() {
-		return "Hello from order service";
+		return msg;
 	}
 	
 }
